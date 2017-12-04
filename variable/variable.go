5aa24b77e910a1faf1e36006fd4ad8accc99eff4
JamesWilson <wesstation2@gmail.com>
2017-12-04 23:32:54 +0800

    加速吧孩子

---
+++ variable/variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 1
+	var b string = "Hello"
+	const pi float32 = 3.1415926
+	fmt.Print(a)
+	fmt.Printf("\n")
+	fmt.Printf(b)
+	fmt.Printf("\n")
+	fmt.Print(pi)
+}
